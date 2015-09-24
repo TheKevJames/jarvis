e148348bd1b42dddfd7091dd797e0c2e9e107a04
Kevin James <KevinJames@thekev.in>
2015-09-24 12:11:30 -0400

    cash_pool: Added history

--- plugins/cash_pool.py
+++ plugins/cash_pool.py
@@ -1,5 +1,6 @@
 """
-You can ask me to "show the cash pool" if you would like to see your debts.
+You can ask me to "show the cash pool" if you would like to see your debts. You
+can also ask me to "show the cash pool history", if you'd prefer.
 Alternatively, you may inform me that "Tom sent $42 to Dick" or that "Tom paid
 $333 for Dick and Harry". Of course, I will always assume that Tom also paid
 for himself.
@@ -23,6 +24,12 @@ try:
 except IOError:
     pool = defaultdict(int)
 
+PICKLE_HISTORY_FILE = 'cash_pool_history.pickle'
+try:
+    history = pickle.load(open(PICKLE_HISTORY_FILE, 'rb'))
+except IOError:
+    history = list()
+
 
 def process_message(data):
     if 'text' in data:
@@ -30,15 +37,24 @@ def process_message(data):
         if not text.startswith('jarvis'):
             return
 
-
         if 'explain the cash pool' in text:
             outputs.append([data['channel'], 'Very well, sir.'])
             outputs.append([data['channel'], __doc__.replace('\n', ' ')])
 
             return
 
-
         if 'show the cash pool' in text:
+            if 'history' in text:
+                if history:
+                    outputs.append([
+                        data['channel'],
+                        'Very good, sir, displaying your history now:'])
+
+                for line in history:
+                    outputs.append([data['channel'], line])
+
+                return
+
             owes, owed = dict(), dict()
             for person, value in pool.iteritems():
                 if value > 0:
@@ -60,7 +76,6 @@ def process_message(data):
 
             return
 
-
         did_send = SENT.match(text)
         if did_send:
             sender, value, sendee = did_send.groups()
@@ -75,6 +90,9 @@ def process_message(data):
             if -0.01 < pool[sendee] < 0.01:
                 pool[sendee] = 0
 
+            history.append(text)
+
+            pickle.dump(history, open(PICKLE_HISTORY_FILE, 'wb'))
             pickle.dump(pool, open(PICKLE_FILE, 'wb'))
 
             outputs.append([data['channel'], 'Very good, sir.'])
@@ -96,6 +114,9 @@ def process_message(data):
                 if -0.01 < pool[payee] < 0.01:
                     pool[payee] = 0
 
+            history.append(text)
+
+            pickle.dump(history, open(PICKLE_HISTORY_FILE, 'wb'))
             pickle.dump(pool, open(PICKLE_FILE, 'wb'))
 
             outputs.append([data['channel'], 'Very good, sir.'])
