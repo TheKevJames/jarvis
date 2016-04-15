880b9cd9831130eecbb843e7b67f5d1ec5839dd2
Kevin James <KevinJames@thekev.in>
2016-04-15 18:20:30 -0400

    scripted send-to-channels

--- send.py
+++ send.py
@@ -3,14 +3,17 @@
 
 Usage:
   send.py --user=<name> <message>...
+  send.py --channel=<name> <message>...
   send.py (-h | --help)
 
 Options:
-  -u --user=<name>  Full name of user to send to
-  -h --help         Show this screen.
+  -u --user=<name>      Full name of user to send to
+  -c --channel=<name>   Channel to send message to
+  -h --help             Show this screen.
 """
 import json
 import os
+import sys
 
 import docopt
 import slackclient
@@ -31,9 +34,15 @@ uuids = {u['profile']['real_name']: u['id'] for u in users['members']}
 icons = {u['profile']['real_name']: u['profile']['image_48']
          for u in users['members']}
 
-channel = json.loads(
-    slack.api_call('im.open', user=uuids[args.get('--user')]))['channel']['id']
+if args.get('--user'):
+    channel = json.loads(slack.api_call(
+        'im.open', user=uuids[args.get('--user')]))['channel']['id']
+elif args.get('--channel'):
+    channel = args.get('--channel')
 
+if not channel:
+    print 'Could not get channel specifier'
+    sys.exit(1)
 
 slack.api_call('chat.postMessage', channel=channel,
                text=' '.join(args.get('<message>')), username=USER,
