0263855ce9689363ddcc1e50ef53e9f8014af10c
Kevin James <KevinJames@thekev.in>
2016-11-27 11:48:26 -0800

    cleanup

--- bot/jarvis/db/channels.py
+++ bot/jarvis/db/channels.py
@@ -10,7 +10,6 @@ class ChannelsDal(dal.Dal):
     def read(cur, admin_only=False):
         users = []
         for u in cur.execute('SELECT channel, is_admin FROM user').fetchall():
-
             if admin_only and not u[1]:
                 continue
 
