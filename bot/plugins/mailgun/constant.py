917bb23b182874e8cbb5ef62e80d26975e19e444
Kevin James <KevinJames@thekev.in>
2016-12-06 00:24:08 -0800

    sent from domain -> sent via domain

--- bot/plugins/mailgun/constant.py
+++ bot/plugins/mailgun/constant.py
@@ -19,9 +19,9 @@ DELETED_FOLLOW = lambda x: \
 
 MAIL_ERROR = 'Sir, there appears to be a problem with your mail server.'
 
-BOUNCED = (MAIL_ERROR + ' A message sent from {} to {} has been bounced. ' + \
+BOUNCED = (MAIL_ERROR + ' A message sent via {} to {} has been bounced. ' + \
     'The SMTP server reports the following error: {}.').format
-COMPLAINED = (MAIL_ERROR + ' A message sent from {} to {} has been '
+COMPLAINED = (MAIL_ERROR + ' A message sent via {} to {} has been '
                            'marked as spam.').format
-DROPPED = (MAIL_ERROR + ' A message sent from {} to {} has been dropped. ' + \
+DROPPED = (MAIL_ERROR + ' A message sent via {} to {} has been dropped. ' + \
     'Mailgun reports it is "{}".').format
